7eb53021e90cd67c55737b523e1324ae174eb586
dgomezlechon <dgomezlechon@gmail.com>
2022-05-16 16:38:48 +0200

    changes

--- my-code/trialst.py
+++ my-code/trialst.py
@@ -108,7 +108,7 @@ for i in range(len(free_cash_flows)):
     
     # we print the values of the variables being used:
     col1, col2, col3 = st.columns(3)
-    col1.metric("Growth Rate", growth_rate[i]*100"%")
+    col1.metric("Growth Rate", growth_rate[i]*100 + "%")
     #col2.metric("WACC", wacc[year][companies_to_use[i]])
     #col3.metric("Humidity", "86%", "4%")
 
